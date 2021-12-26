948085612765f670d66c8ee35707b7f34b74c57b
alnu <avinash.lnu@greenwayhealth.com>
2021-12-26 16:34:38 +0530

    Added custom sorting

--- AngularFundamentals/src/app/events/event-details/session-list.component.ts
+++ AngularFundamentals/src/app/events/event-details/session-list.component.ts
@@ -15,12 +15,14 @@ import { ISession } from "../shared";
 export class SessionListComponent implements OnChanges {
  
  @Input() filterBy: string;
+ @Input() sortBy: string;
  @Input() sessions: ISession[];
  visibleSessions: ISession[] = [];
 
  ngOnChanges() {
     if(this.sessions){
       this.filterSession(this.filterBy);
+      this.sortBy === 'name' ? this.visibleSessions.sort(sortByNameAsc): this.visibleSessions.sort(sortByVotesDesc)
     }
   }
   filterSession(filter){
@@ -33,6 +35,15 @@ export class SessionListComponent implements OnChanges {
       })
     }
   }
+}
+
+function sortByNameAsc(s1:ISession, s2:ISession){
+  if(s1.name > s2.name) return 1;
+  else if(s1.name === s2.name) return 0;
+  return -1;
+}
+function sortByVotesDesc(s1:ISession, s2:ISession){
+  return s1.voters.length - s2.voters.length ;
 }
 
   
